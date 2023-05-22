9cb0332e7d0cdf9c138c65f0b2d77cc736a4eed4
evg007 <evg007554@yandex.ru>
2023-05-23 02:29:09 +0300

    230

--- src/stack.py
+++ src/stack.py
@@ -7,7 +7,8 @@ class Node:
 
         :param data: данные, которые будут храниться в узле
         """
-        pass
+        self.data = data
+        self.next_node = next_node
 
 
 class Stack:
@@ -15,7 +16,8 @@ class Stack:
 
     def __init__(self):
         """Конструктор класса Stack"""
-        pass
+        self.top = None
+
 
     def push(self, data):
         """
@@ -23,7 +25,14 @@ class Stack:
 
         :param data: данные, которые будут добавлены на вершину стека
         """
-        pass
+
+        if self.top:
+            nn = Node(data,self.top)
+            # nn.next_node =
+        else:
+            nn = Node(data,None)
+        self.top = nn
+
 
     def pop(self):
         """
